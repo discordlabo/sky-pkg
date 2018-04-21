65002636c4f1f595a66d0b016fc1aa578632bea5
Ken Shibata <35400564+python3lover@users.noreply.github.com>
2018-04-20 20:11:57 -0400

    Create core.py

---
+++ pkg_feature_test/core.py
@@ -0,0 +1,8 @@
+class io_base():
+  def __init__(self,version):
+    pass
+  def call(self,message):
+    pass
+class io(io_base):
+  def __init__(self):
+    print('i [pkg_feature_test] Packages Feature Working')
