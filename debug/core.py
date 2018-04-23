50cc2445147fba2473a08f44c2ff9aa6cd86260d
Ken Shibata <35400564+python3lover@users.noreply.github.com>
2018-04-23 08:11:20 -0400

    Update core.py

--- debug/core.py
+++ debug/core.py
@@ -1,5 +1,7 @@
 class io():
-  def __init__(self):
+  def __init__(self,version):
     print('  [builtin-debug] Debug Package')
   def call(self,message):
     print('i [builtin-debug] Received Message: {}\nChannel: {}'.format(message.content,message.channel))
+  def name(self):
+    return 'Debug'
