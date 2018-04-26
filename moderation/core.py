f7a5a5b2121b74f9feec1c7f9ebe90f62f17ae02
Ken Shibata <35400564+python3lover@users.noreply.github.com>
2018-04-25 21:57:05 -0400

    Warning System 0.1.0 Stable
    Warning System 0.1.0 Stable (sky-b41) (sky_moderation)
    
    A package for imitating what @Dyno#3861's moderation techniques do.

--- moderation/core.py
+++ moderation/core.py
@@ -2,8 +2,15 @@ class io():
   def __init__(self,version,bot=None):
     self.version = version
     self.bot = bot
-    self.name = 'Warning System'
+    self.name = 'Warning System 0.1.0 Stable'
   def call(self,message):
-    pass
+    if message.content.startswith(':warn/'):
+      yield from bot.send_message(message.channel,':warning: Warning Feature is not available now. :pensive:')
+    elif message.content.startswith(':ban/'):
+      yield from bot.send_message(message.channel,':warning: Ban Feature is not available now. :pensive:')
+    elif message.content.startswith(':kick/'):
+      yield from bot.send_message(message.channel,':warning: Kick Feature is not available now. :pensive:')
+    elif message.content.startswith(':reset/'):
+      yield from bot.send_message(message.channel,':warning: Reset Feature is not available now. :pensive:')
   def name(self):
     reurn self.name
