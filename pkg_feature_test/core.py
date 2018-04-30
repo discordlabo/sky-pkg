2eaafc4e362730d8130f675ecd83b4d068b064f0
Ken Shibata <35400564+python3lover@users.noreply.github.com>
2018-04-29 21:41:46 -0400

    Update core.py

--- pkg_feature_test/core.py
+++ pkg_feature_test/core.py
@@ -1,8 +1,7 @@
-class io_base():
-  def __init__(self,version,bot=None):
-    pass
+class io():
+  def __init__(self,version,bot):
+    print('i [pkg_feature_test] Packages Feature Working')
   def call(self,message):
     pass
-class io(io_base):
-  def __init__(self,version):
-    print('i [pkg_feature_test] Packages Feature Working')
+  def name(self):
+    return 'Package Feature Test'
